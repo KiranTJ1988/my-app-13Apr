a69275ad3836dae8673a639069ba7b9fe8a3dac3
KiranTJ1988 <45386831+KiranTJ1988@users.noreply.github.com>
2023-04-13 07:56:34 +0530

    Added jenkins

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! by KIRAN from Mmaven" );
+        System.out.println( "Hello World! by KIRAN from Mmaven & Jenkins" );
     }
 }
